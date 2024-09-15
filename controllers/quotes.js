aebefedcfc66bbc52a38026e4fe91eb8234e472f
Larysa Rud <larpavrud@gmail.com>
2024-09-15 19:09:30 -0400

    Creating a functionality to get a random quote from a database

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,9 +1,8 @@
 const Quote = require('../models/Quote');
 
 module.exports = {
-  getAll
+  getAll, getRandom
 };
-
 
 // getAll function retrieves all quotes from the database
 async function getAll(req, res) {
@@ -12,6 +11,22 @@ async function getAll(req, res) {
     if (quotes) {
       res.status(200).json(quotes);
     }
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+// getRandom function retrieves random quote from the database
+async function getRandom(req, res) {
+  try {
+    const count = await Quote.countDocuments();
+    if (count === 0) {
+      return res.status(404).send('No quotes found');
+    }
+    const randomIndex = Math.floor(Math.random() * count);
+    const randomQuote = await Quote.findOne().skip(randomIndex);
+    res.status(200).json(randomQuote);
+
   } catch (err) {
     res.status(400).send(err);
   }
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { getAll } = require('../controllers/quotes.js');
+
+const { getAll, getRandom } = require('../controllers/quotes.js');
 
 const router = Router();
 
-
-
 router.get('/', getAll);
+
+router.get('/random', getRandom);
 
 module.exports = router;
