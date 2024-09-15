3136e1d89b63be89a6d2fe3ffcd576360362c738
Larysa Rud <larpavrud@gmail.com>
2024-09-15 19:59:17 -0400

    Changing the order of middleware for cors to work

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 // Requiring and configuring the .env file to access its variables
 require('dotenv').config();
+
 // Requiring express
 const express = require('express');
+
 // Creating the express server and storing inside the app variable
 const app = express();
+
 // Port in which the server will run on
 const PORT = process.env.PORT || 8000;
+
 // Requiring example router
 const quoteRouter = require('./routes/quotes.js');
 
 // Configuring the server to accept and parse JSON data.
 app.use(express.json());
+
+//Importing the cors middleware
+const cors = require('cors');
+
+// Applying the cors middleware to the entire Express application, 
+//allowing it to respond to cross-origin requests
+app.use(cors());
 
 //Custom Middlware
 app.use((req, res, next) => {
@@ -25,13 +36,6 @@ app.use('/quotes', quoteRouter);
 app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong.');
 });
-
-//Importing the cors middleware
-const cors = require('cors');
-
-// Applying the cors middleware to the entire Express application, 
-//allowing it to respond to cross-origin requests
-app.use(cors());
 
 // Calling the listen function telling the server to listen on port 3000
 app.listen(PORT, () => {
